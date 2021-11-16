6116b42da0064a9bafcedc5aeb0e33bc10605391
mridul1024 <mridul1024@gmail.com>
2021-11-16 14:23:05 +0530

    Base Application

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <div>
+          <p>
+            React application created using custom webpack configuration file
+          </p>
+          <p>Finally its working !!</p>
+          <h1>learning to work with custom webpack configuration feels good</h1>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
